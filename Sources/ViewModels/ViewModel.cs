9a166329b0455509f66552f2ca4d5e040d713fda
avatar <aBaTaPbl4@yandex.ru>
2013-06-10 03:33:14 +0400

    Перенес вью модели в отдельную сборку

--- Sources/ViewModels/ViewModel.cs
+++ Sources/ViewModels/ViewModel.cs
@@ -1,10 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
-using System.Text;
 using System.Windows;
-using System.Windows.Threading;
+
 
 namespace Searcher.VM
 {
